84fdce9495b6981b078cd5b5ac431d1ade70e476
Eric-Barajas <barajaseric505@gmail.com>
2023-03-17 21:14:17 -0700

    init commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+import Navabar from "scenes/navbar";
+
+const HomePage = () => {
+    return (
+        <Box>
+            <Navabar />
+        </Box>
+    );
+};
+
+export default HomePage;
--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS (middleware something that runs in between requests) //
 const __filename = fileURLToPath(import.meta.url);
@@ -41,10 +47,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES //
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES //
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP //
 const PORT = process.eventNames.PORT || 6001;
@@ -53,4 +61,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
+
+    // ADD DATA ONE TIME//
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+})
+    .catch((error) => console.log(`${error} did not connect`));
